b8b274da120e464c18baf04b2f105f6de9c9d836
rodrigonahid <rodrigonahid1@gmail.com>
2021-11-21 00:21:46 -0300

    initial commig

---
+++ backend/src/controllers/AuthController.js
@@ -0,0 +1,81 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserSchema = require("../models/user");
+
+const AuthController = {
+  register: async (req, res) => {
+    const { name, email, password, confirmPassword } = await req.body;
+    if (
+      name == null ||
+      email == null ||
+      password == null ||
+      confirmPassword == null
+    ) {
+      return res.status(400).json({ error: "Fill the form" });
+    }
+    if (password != confirmPassword) {
+      return res.status(400).json({ error: "Passwords doesn't match" });
+    }
+    const emailExists = await UserSchema.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+    // Create password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new UserSchema({
+      name,
+      email,
+      password: passwordHash,
+    });
+    try {
+      const newUser = await user.save();
+
+      const token = jwt.sign(
+        {
+          name: newUser.name,
+          id: newUser.id,
+        },
+        "912ec803b2ce49e4a541068d495ab570"
+      );
+      res.json({
+        error: null,
+        msg: "Voce realizou o cadastro com sucesso",
+        token: token,
+        userId: newUser.id,
+      });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await UserSchema.findOne({ email: email });
+    console.log(user.email);
+    if (!user) {
+      return res.status(400).json({ err: "Email not registered" });
+    }
+    const checkPassword = await bcrypt.compare(password, user.password);
+    console.log(checkPassword);
+    if (!checkPassword) {
+      return res.status(400).json({ err: "Invalid password" });
+    }
+    const token = jwt.sign(
+      {
+        name: user.name,
+        id: user.id,
+      },
+      "912ec803b2ce49e4a541068d495ab570"
+    );
+    res.json({
+      error: null,
+      msg: "Voce esta autenticado",
+      token: token,
+      userId: user.id,
+    });
+  },
+};
+
+module.exports = AuthController;
---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes");
+const app = express();
+
+// app.use(cors);
+app.use(express.json());
+app.use(express.static("public"));
+// Conexao mongodb
+// Routes
+app.use(routes);
+mongoose.connect("mongodb://localhost/partyTime");
+
+app.listen(8001);
