24bae457ae324e71768ca3af687e3918f8bb7cfd
rodrigonahid <rodrigonahid1@gmail.com>
2021-11-24 19:50:08 -0300

    finished backend mvp

--- backend/src/controllers/PartyController.js
+++ backend/src/controllers/PartyController.js
@@ -64,6 +64,111 @@ const PartyController = {
       res.status(400).json({ err });
     }
   },
+  getUser: async (req, res) => {
+    try {
+      const token = req.header("auth-token");
+      const user = await getUserByToken(token);
+      const userId = user._id.toString();
+      const parties = await Party.find({ userId: userId });
+      res.json({ error: null, parties: parties });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
+  },
+  getOnlyUser: async (req, res) => {
+    try {
+      const token = req.header("auth-token");
+      const user = await getUserByToken(token);
+      const userId = user._id.toString();
+      const partyId = req.params.id;
+      const party = await Party.findOne({ _id: partyId, userId: userId });
+      res.json({ err: null, party: party });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
+  },
+  getPrivacy: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const party = await Party.findOne({ _id: id });
+
+      if (party.privacy === false) {
+        res.json({ err: null, party: party });
+      } else {
+        const token = req.header("auth-token");
+        const user = await getUserByToken(token);
+
+        const userId = user._id.toString();
+        const partyUserId = party.userId.toString();
+        if (userId == partyUserId) {
+          res.json({ err: null, party: party });
+        } else {
+          res.status(400).json({ err: "Access denied" });
+        }
+      }
+    } catch (err) {
+      res.status(400).json({ msg: "Event doesn't exists" });
+    }
+  },
+  update: async (req, res) => {
+    const { title, description, partyDate, partyId, partyUserId } = req.body;
+    const token = req.header("auth-token");
+    let files = [];
+
+    if (req.files) {
+      files = req.files.photos;
+    }
+
+    if (title == null || description == null || partyDate == null) {
+      return res.status(400).json({ error: "Fill the fields" });
+    }
+    const userByToken = getUserByToken(token);
+    const userId = userByToken._id;
+    if (userId != partyUserId) {
+      return res.status(400).json({ err: "Access denied" });
+    }
+
+    const party = {
+      id: partyId,
+      title: title,
+      description: description,
+      partyDate: partyDate,
+      privacy: req.body.privacy,
+      userId: userId,
+    };
+
+    let photos = [];
+    if (files && files.length > 0) {
+      files.forEach((photo, i) => {
+        photos[i] = photo.path;
+      });
+      party.photos = photos;
+    }
+
+    try {
+      const updatedParty = await Party.findOneAndUpdate(
+        { _id: partyId, userId: userId },
+        { $set: { party } },
+        { new: true }
+      );
+      res.json({ err: false, party: updatedParty });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
+  },
+  delete: async (req, res) => {
+    const token = req.header("auth-token");
+    const user = await getUserByToken(token);
+    const partyId = req.body.id;
+    const userId = user._id.toString();
+
+    try {
+      await Party.deleteOne({ _id: partyId, userId: userId });
+      res.json({ err: null, msg: "Evento removido com sucesso" });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
+  },
   multer: upload.fields({ name: "photos" }),
 };
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,11 +12,21 @@ router.get("/user/:id", TokenMiddleware, UserController.get);
 router.put("/user", TokenMiddleware, UserController.update);
 
 router.get("/party", PartyController.get);
+router.get("/party/user", TokenMiddleware, PartyController.getUser);
+router.get("/party/user/:id", TokenMiddleware, PartyController.getOnlyUser);
+router.get("/party/:id", PartyController.getPrivacy);
 router.post(
   "/party",
   TokenMiddleware,
   PartyController.multer,
   PartyController.create
 );
+router.put(
+  "/party",
+  TokenMiddleware,
+  PartyController.multer,
+  PartyController.update
+);
+router.delete("/party", TokenMiddleware, PartyController.delete);
 
 module.exports = router;
