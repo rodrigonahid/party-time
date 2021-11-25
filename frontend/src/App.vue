fc3e300c874008cd2b953105b16bc1925376c8f7
rodrigonahid <rodrigonahid1@gmail.com>
2021-11-25 16:37:19 -0300

    frontend init

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <router-link to="/">Teste</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
