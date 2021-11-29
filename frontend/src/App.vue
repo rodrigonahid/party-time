6485b5784a55f6d680cb30467847f900158d8dff
rodrigonahid <rodrigonahid1@gmail.com>
2021-11-28 21:59:29 -0300

    trying to fix webpack

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,26 @@
 <template>
-  <div>
-    <Navbar />
-    <Footer />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    Navbar,
-    Footer,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- frontend/src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer>This is the footer</footer>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
